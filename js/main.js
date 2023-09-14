46ded351bc8b37fd33ec7fcdfee2bd9df9679962
Dasha <vasutadasha25@gmail.com>
2023-09-14 18:11:42 +0300

    Commits

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
-// Отримуємо посилання на елементи DOM
 const menuBurgerBlock = document.querySelector('.menu-burger-block');
 const menuBurger = document.querySelector('.menu-burger');
 const menuHeader = document.querySelector('.header-menu');
 
-
-// Додаємо обробник події при кліку на menuBurger
 menuBurger.addEventListener('click', () => {
     // Додаємо або видаляємо клас "active" для menuBurgerBlock та menuBurger
     menuBurgerBlock.classList.toggle('active');
@@ -14,20 +11,91 @@ menuBurger.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', function () {
     const mySwiper = new Swiper('.slider-container', {
-        slidesPerView: 1, // Кількість видимих слайдів одночасно
-        spaceBetween: 20, // Відстань між слайдами
+        slidesPerView: 1,
+        spaceBetween: 20,
         loop: true,
         navigation: {
-            nextEl: '.swiper-button-next', // Селектор кнопки "Наступний слайд"
-            prevEl: '.swiper-button-prev', // Селектор кнопки "Попередній слайд"
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
         autoplay: {
-            delay: 4000, // Затримка між автоматичною зміною слайдів в мілісекундах (4 секунди)
-            disableOnInteraction: false, // Вимкнути автопрокрутку при взаємодії користувача
+            delay: 4000,
+            disableOnInteraction: false,
         },
-        effect: 'fade', // Ефект переходу між слайдами
+        effect: 'fade',
         fadeEffect: {
-            crossFade: true, // Зміна зображення через згасання (fade)
+            crossFade: true,
+        },
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const nameInput = document.getElementById('name-input');
+    const phoneInput = document.getElementById('phone-input');
+    const emailInput = document.getElementById('email-input');
+    const submitButton = document.getElementById('submit-button');
+    const form = document.getElementById('feedback-form');
+    const modal = document.getElementById('popup');
+
+    function validateField(input, pattern, errorMessage) {
+        const value = input.value.trim();
+        if (value === "") {
+            input.placeholder = "Поле обов'язкове для введення";
+            input.classList.add('invalid');
+            input.style.border = "2px solid rgba(228, 106, 106, 0.75)";
+            return false;
+        } else if (!pattern.test(value) || (input.id === 'name-input' && !isFirstLetterUpperCase(value))) {
+            input.placeholder = errorMessage;
+            input.value = "";
+            input.classList.add('invalid');
+            input.style.border = "2px solid rgba(228, 106, 106, 0.75)";
+            return false;
+        } else {
+            input.textContent = "";
+            input.style.boxShadow = "";
+            input.classList.remove('invalid');
+            input.style.border = "2px solid #ccc";
+            return true;
+        }
+    }
+
+    function isFirstLetterUpperCase(str) {
+        return /^[A-ZА-ЯЁІЇЄҐ]/.test(str);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const isNameValid = validateField(nameInput, /^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ\s'-]+$/, "Неправильний формат введення даних");
+        const isEmailValid = validateField(emailInput, /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, "Неправильний формат Email");
+        const isPhoneValid = validateField(phoneInput, /^\d{12}$/, "Неправильний формат введення даних");
+
+        if (isNameValid && isEmailValid && isPhoneValid) {
+            form.submit();
+            modal.style.display = 'none';
+        }
+    }
+
+    submitButton.addEventListener('click', handleSubmit);
+
+    nameInput.addEventListener('focus', function () {
+        if (nameInput.classList.contains('invalid')) {
+            nameInput.classList.remove('invalid');
+            nameInput.style.border = "2px solid #ccc";
+        }
+    });
+
+    emailInput.addEventListener('focus', function () {
+        if (emailInput.classList.contains('invalid')) {
+            emailInput.classList.remove('invalid');
+            emailInput.style.border = "2px solid #ccc";
+        }
+    });
+
+    phoneInput.addEventListener('focus', function () {
+        if (phoneInput.classList.contains('invalid')) {
+            phoneInput.classList.remove('invalid');
+            phoneInput.style.border = "2px solid #ccc";
         }
     });
 });
